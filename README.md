faa7d9d7b977ed5a4d589ee079c23ebcf8f9ac30
dev-cardenas <dev.cardenas@gmail.com>
2022-11-08 12:31:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Test to EDGE
+
+This is a basic product where you can authenticate and save product
+
+## To run this project
+### Requirements
+  - node v14.17.1
+  - npm or yarn
+
+#### To run in dev
+```
+yarn install
+yarn dev
+```
+you can see the documentation in here http://localhost:5001/api/v1/api-docs/
+once you've run the service for a complete guide to what you can do
---
+++ src/lib/docs.ts
@@ -0,0 +1,103 @@
+import swaggerJSDoc, { OAS3Definition, OAS3Options } from "swagger-jsdoc";
+// const swaggerDoc = require('../../swagger.json')
+
+const swaggerDefinition: OAS3Definition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Example Api to EDGE",
+    description: "Basic Documentation",
+    license: {
+      name: "MIT",
+      url: "http://localhost:5001",
+    },
+    termsOfService: "Beautiful",
+    version: "1.0.0",
+  },
+  servers: [
+    {
+      url: "http://localhost:5001/api/v1",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+      },
+    },
+    tags: {
+      name: "products",
+      description: "Everything about your products",
+    },
+    schemas: {
+      user: {
+        type: "object",
+        required: ["email", "password"],
+        properties: {
+          email: {
+            type: "string",
+          },
+          password: {
+            type: "string",
+          },
+        },
+      },
+      product: {
+        type: "object",
+        required: ["name", "price", "quantity", "code"],
+        properties: {
+          name: {
+            type: "string",
+          },
+          price: {
+            type: "number",
+          },
+          quantity: {
+            type: "number",
+          },
+          code: {
+            type: "number",
+          },
+        },
+      },
+      product_to_update: {
+        type: "object",
+        required: ["id"],
+        properties: {
+          id: {
+            type: "number",
+          },
+          name: {
+            type: "string",
+          },
+          price: {
+            type: "number",
+          },
+          quantity: {
+            type: "number",
+          },
+          code: {
+            type: "number",
+          },
+        },
+      },
+      product_id: {
+        type: "object",
+        required: ["id"],
+        properties: {
+          id: {
+            type: "number",
+          },
+        },
+      },
+    },
+  },
+};
+
+const swaggerOptions: OAS3Options = {
+  swaggerDefinition,
+  apis: ["./src/routes/*.ts"],
+};
+
+export default swaggerJSDoc(swaggerOptions);
